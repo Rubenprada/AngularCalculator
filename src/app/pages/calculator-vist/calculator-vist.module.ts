585c3d69a45521600256ce9195aa285e92b48068
Rubenprada <rubenprada89@outlook.com>
2023-04-13 11:32:19 +0200

    FEAT: create new calculator

---
+++ src/app/pages/calculator-vist/calculator-vist.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CalculatorVistRoutingModule } from './calculator-vist-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CalculatorVistRoutingModule
+  ]
+})
+export class CalculatorVistModule { }
